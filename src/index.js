b018b1f6f1dd5942075920152856e039a2ece3f7
Landerar <lander.arbelaiz@gmail.com>
2024-05-21 00:28:39 +0200

    el contenedor y el volumen no pueden tener el mismo nombre, añadido el archivo de configuracion mongo.js e importado al index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./config/mongo.js";
 
 dotenv.config();
 const CONTAINER_PORT = 3000;
 
 const app = express();
-
+connectDB();
 app.get("/",(req,res)=>{
     res.json({message: "Hello World"});
 })
