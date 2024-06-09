092cea8779e1fca4dde507de257c882b14b70eef
Lander <lander.arbelaiz@gmail.com>
2024-06-09 10:47:05 +0200

    navbar creation and integration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,15 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import ExcuseGenerator from "./components/ExcuseGenerator.jsx";
-import ExcuseList from "./components/ExcuseList.jsx";
 import LoginForm from "./components/LoginForm.jsx";
 import RegisterForm from "./components/RegisterForm.jsx";
-// import Navbar from "./components/Navbar.jsx";
+import Navbar from "./components/Navbar/Navbar.jsx";
 
 function App(){
   return(
     <Router>
+      <Navbar/>
       <Routes>
-        <Route path="/" element={<ExcuseList/>}/>
+        <Route path="/" element={<home/>}/>
         <Route path="/register" element={<RegisterForm/>}/>
         <Route path="/login" element={<LoginForm/>}/>
         <Route path="/excuse" element={<ExcuseGenerator/>}/>
